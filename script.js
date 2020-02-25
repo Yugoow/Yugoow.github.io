7737177c5aa0abac5c4f35da4ed270b9f72a57fb
Hugo Breniaux <56593824+Yugoow@users.noreply.github.com>
2020-02-25 14:10:50 +0100

    tournerbtn
    animation css

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
 let but2 = document.getElementById("but2");
 
 let id_div_cache_2 = document.getElementById("id_div_cache_2");
-
+id_div_cache_2.style.display = "none";
 
 function but(){
-  if(getComputedStyle(id_div_cache_2).display != "none"){
-    id_div_cache_2.style.display = "none";
-  } else {
+  if(getComputedStyle(id_div_cache_2).display == "none"){
     id_div_cache_2.style.display = "block";
   }
 };
