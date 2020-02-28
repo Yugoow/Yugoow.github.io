8621ce6216c1da3785a93e440619cc15056462a6
Hugo Breniaux <56593824+Yugoow@users.noreply.github.com>
2020-02-28 12:15:11 +0100

    sous menu

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 let but2 = document.getElementById("but2");
-
 let id_div_cache_2 = document.getElementById("id_div_cache_2");
+let langage = document.getElementById("langage");
+var cacher ="False";
 id_div_cache_2.style.display = "none";
+
 
 function but(){
   if(getComputedStyle(id_div_cache_2).display == "none"){
@@ -9,8 +11,21 @@ function but(){
   }
 };
 
+
+function men(){
+let menu = document.getElementById("1")
+  if(cacher != "False"){
+    menu.style.height="0"
+    cacher = "False"
+  } else {
+  	menu.style.height="70px"
+  	cacher = "True"
+  }
+
+};
+
+but2.onclick = but
+langage.onclick = men;
 /*but2.addEventListener("mouseover",function(event){
 	document.getElementsById("but2").animate()
 }) */
-
-but2.onclick = but;
