42653157dee5ea340b2d4176deaac6c05c442f50
Hugo Breniaux <56593824+Yugoow@users.noreply.github.com>
2020-03-03 16:20:47 +0100

    devlpmnt of menu (js)

--- Accueil/script.js
+++ Accueil/script.js
@@ -4,7 +4,6 @@
 
 let but2 = document.getElementById("but2");
 let id_div_cache_2 = document.getElementById("id_div_cache_2");
-
 id_div_cache_2.style.display = "none";
 
 function but(){
@@ -15,82 +14,101 @@ function but(){
 
 
 
+                                   /*  Menu pas opti    */
 
 
+/* Création des variables  */
 
-
-/*  Menu pas opti    */
 men1="False"
 men2="False"
 men3="False"
 men4="False"
 
+/*Récup bouttons par id*/
 let div1 = document.getElementById("1");
 let menu1 = document.getElementById("actuellement");
 
-function test1(){
+let menu2 = document.getElementById("reseau");
+let div2 = document.getElementById("2");
+
+let menu3 = document.getElementById("langage");
+let div3 = document.getElementById("3");
+
+let menu4 = document.getElementById("projets");
+let div4 = document.getElementById("4");
+
+function deroulmenu(){
+
+  /*Récupération de l'id boutton*/
+  let a =this.id;
+
+
+  /*premier bouton*/
+
+    if(a=="actuellement"){
   if(men1 == "False"){
     div1.style.height="75px";
     div1.style.border="none";
     men1="True"
   } else{
-  	div1.style.height="0px";
+    div1.style.height="0px";
     div1.style.border="1px solid black";
-  	men1="False"
+    men1="False"
   }
-}
+  }
 
 
-let menu2 = document.getElementById("reseau");
-let div2 = document.getElementById("2");
-function test2(){
+
+  /*Deuxième bouton*/
+
+    if(a=="reseau"){
   if(men2 == "False"){
     div2.style.height="75px";
     div2.style.border="none";
     men2="True"
   } else{
-  	div2.style.height="0px";
+    div2.style.height="0px";
     div2.style.border="1px solid black";
-  	men2="False"
+    men2="False"
   }
-}
+  }
 
 
+  /*troisième bouton*/
 
-let menu3 = document.getElementById("langage");
-let div3 = document.getElementById("3");
-function test3(){
+    if(a=="langage"){
   if(men3 == "False"){
     div3.style.height="75px";
     div3.style.border="none";
     men3="True"
   } else{
-  	div3.style.height="0px";
+    div3.style.height="0px";
     div3.style.border="1px solid black";
-  	men3="False"
+    men3="False"
   }
-}
+  }
 
 
-let menu4 = document.getElementById("projets");
-let div4 = document.getElementById("4");
-function test4(){
+  /*Quatrième bouton*/
 
+    if(a=="projets"){
   if(men4 == "False"){
     div4.style.height="75px";
     div4.style.border="none";
     men4="True"
   } else{
-  	div4.style.height="0px";
+    div4.style.height="0px";
     div4.style.border="1px solid black";
-  	men4="False"
+    men4="False"
+  }
   }
 }
 
-menu1.onclick =test1;
-menu2.onclick =test2;
-menu3.onclick =test3;
-menu4.onclick =test4;
+/*Appel de la fonction au clic*/
+menu1.onclick =deroulmenu;
+menu2.onclick =deroulmenu;
+menu3.onclick =deroulmenu;
+menu4.onclick =deroulmenu;
 
 
 
