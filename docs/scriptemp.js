7fb0cb5d5e044419fe20523842f1b429494b0185
Hugo Breniaux <56593824+Yugoow@users.noreply.github.com>
2020-03-22 00:25:01 +0100

    New design website
    New design for teh websit in the doc document

---
+++ docs/scriptemp.js
@@ -0,0 +1,13 @@
+/*
+
+window.onscroll = Scrollmenu();
+
+function Scrollmenu() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 520) {
+    document.getElementById("container").className = "test";
+  } else {
+    document.getElementById("container").className = "";
+  }
+}
+
+*/
