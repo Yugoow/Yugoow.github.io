8a855e3b26534c36cfb371184689b602bbf15c1a
Hugo Breniaux <56593824+Yugoow@users.noreply.github.com>
2020-04-10 00:38:51 +0200

    amelioration des fontes
    fonte ++ zone com ?
    Est-ce que je laisse les articles ou pas vu que c'est pas en dynamique, dcp pq pas un petit sommaire a la place

--- docs/scriptemp.js
+++ docs/scriptemp.js
@@ -1,5 +1,25 @@
 /*
 
+window.onload = dessin();
+function dessin() {
+	  var canevas = document.getElementById('canevas');
+  if (canevas.getContext) {
+    var ctx = canevas.getContext('2d');
+
+    // Exemples de courbes quadratiques
+    ctx.beginPath();
+    ctx.moveTo(75, 25);
+    ctx.quadraticCurveTo(25, 25, 25, 62.5);
+    ctx.quadraticCurveTo(25, 100, 50, 100);
+    ctx.quadraticCurveTo(50, 120, 30, 125);
+    ctx.quadraticCurveTo(60, 120, 65, 100);
+    ctx.quadraticCurveTo(125, 100, 125, 62.5);
+    ctx.quadraticCurveTo(125, 25, 75, 25);
+    ctx.stroke();
+  }
+}
+
+
 window.onscroll = Scrollmenu();
 
 function Scrollmenu() {
